70ba24fc91cec5fe9df214b46b293d2fc1a4f1e9
tharindulmnop <your.email@example.com>
2024-11-09 18:49:08 +0530

    added faker for random name gen

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,21 @@
 import ContactPage from "../support/ContactPage"
+import { faker } from '@faker-js/faker';
+
+
 describe('Restful Booker', () => {
   it('Send Message Admin', () => {
     
+    const fname=faker.internet.username(); 
+    const faddress=faker.internet.email(); 
+    const fcontact=faker.phone.number('###-###-###-###'); 
+    const fsubject=faker.lorem.sentence();
+    const fmessage=faker.lorem.sentence();
+
+
     ContactPage.visit();
     ContactPage.VerifyLogging();
-    ContactPage.fillContactForm(
-      'Tharindu Madusanka',
-      'Tharindu@example.com',
-      '0755252523651',
-      'Practice Automation here with demo',
-      'Hi, I want to practice some automation scripts here'
-    );
+    ContactPage.fillContactForm(fname,faddress,fcontact,fsubject,fmessage);
+  
     cy.wait(2000);
     ContactPage.submitForm();
     cy.wait(3000)
