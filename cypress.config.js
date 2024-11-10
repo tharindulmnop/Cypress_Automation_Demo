2f87d5b24216b4586e9cea4190f6d55965d8bd32
tharindulmnop <your.email@example.com>
2024-11-10 10:18:25 +0530

    added reports generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Any additional event listeners can go here if needed
     },
-  },
+    // Reporter setup
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports', // Directory where the reports will be saved
+      overwrite: true,              // Overwrite existing report files
+      html: true,                   // Generate HTML report
+      json: true                    // Generate JSON report (used for merging)
+    }
+  }
 });
